2c05187e850850dc1d331859bfa6e6d5b26b0813
Brian <blsvsanteliz@gmail.com>
2020-12-09 18:52:40 -0400

    Modify middleware2

--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -4,21 +4,18 @@ module.exports = class AuthToken {
   static adminAccess(req, res, next) {
     try {
       const tokenHeader = req.headers["auth-token"];
-      console.log(tokenHeader);
       if (!tokenHeader) {
         return res.status(401).json("No existe token");
       }
       const tokenDedoce = jwt.verify(tokenHeader, process.env.SECRET_KEY);
-      if (!tokenDedoce) {
-        res.status(403).json("Token es invalido");
-        return next();
-      }
       const { id } = tokenDedoce;
       req.adminId = id;
       next();
     } catch ({ message }) {
       if (message === "jwt expired") {
         res.status(403).json("El Token Expiró");
+      } else {
+        res.status(403).json("Este token es invalido");
       }
     }
   }
