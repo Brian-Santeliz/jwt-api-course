2a7a529c74eb74f1e3f944d16b1adc3619814435
Brian <blsvsanteliz@gmail.com>
2020-12-09 13:30:38 -0400

    Change router

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -2,8 +2,7 @@ const { Router } = require("express");
 const ControllerAuth = require("../controller/auth.controller");
 const controller = new ControllerAuth();
 const router = Router();
-//crear la ruta ger aparte
+router.get("/admin", controller.registerGetController);
 router.post("/register", controller.registerController);
-router.get("/register", controller.registerGetController);
 router.post("/login", controller.loginController);
-module.exports = router;
+module.exports = routelr;
